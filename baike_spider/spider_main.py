89e9b81dadb8de453b9920b22e1f7779f61b1739
tangjinr <tangpersonal@163.com>
2016-04-22 13:48:53 +0800

    版本1.0

---
+++ baike_spider/spider_main.py
@@ -0,0 +1,36 @@
+# coding:utf8
+# 调度程序
+import url_manager, html_downloader, html_parser, html_outputer
+
+
+class SpiderMain(object):
+    def __init__(self):
+        # url 管理器， 模板下载器，下载数据解析器，解析数据输入器。 模块调度器
+        self.urls = url_manager.UrlManager()
+        self.downloader = html_downloader.HtmlDownloader()
+        self.parser = html_parser.HtmlParser()
+        self.outputer = html_outputer.HtmlOutputer()
+
+    def craw(self, root_url):
+        count = 1
+        self.urls.add_new_url(root_url)
+        while self.urls.has_new_url():
+            try:
+                new_url = self.urls.get_new_url()
+                print('craw %d : %s' % (count, new_url))
+                html_cont = self.downloader.download(new_url)
+                new_urls, new_data = self.parser.parse(new_url, html_cont)
+                self.urls.add_new_urls(new_urls)
+                self.outputer.collect_data(new_data)
+                if count == 100:
+                    break
+                count += 1
+            except:
+                print('craw failed')
+        self.outputer.output_html()
+
+
+if __name__ == "__main__":
+    root_url = "http://baike.baidu.com/view/21087.htm"
+    obj_spider = SpiderMain()
+    obj_spider.craw(root_url)
